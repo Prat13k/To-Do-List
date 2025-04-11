05a1417d3b8fa48e959871f9c4077a03f417bad2
Pratik Patil <ppratik1399@gmail.com>
2025-04-12 00:41:52 +0530

    new changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,17 @@
 import adapter from '@sveltejs/adapter-auto';
-import {vitePreprocess} from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess';
+import path from 'path';
 
-export default{
-	kit : {
-		adapter : adapter(),
-		alias : {
-			$lib :'./src/lib',
-			$components :'./src/lib/components',
-			$stores :'./src/lib/stores',
-			$utils :'./src/lib/utils',
-			$styles :'./src/lib/styles'
-		}
-	},
-	preprocess: vitePreprocess()
+export default {
+  kit: {
+    adapter: adapter(),
+    alias: {
+      $lib: path.resolve('./src/lib'),
+      $components: path.resolve('./src/lib/components'),
+      $stores: path.resolve('./src/lib/stores'),
+      $utils: path.resolve('./src/lib/utils'),
+      $styles: path.resolve('./src/lib/styles')
+    }
+  },
+  preprocess: preprocess()
 };
